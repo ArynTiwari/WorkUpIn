7e848c9ce850b0b099256aafcc88578de7cbfcda
Aryan <arutiwari500@gmail.com>
2023-01-23 21:41:14 +0530

    removed authSHowcase

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import { type NextPage } from "next";
 import Head from "next/head";
-import { signIn, signOut, useSession } from "next-auth/react";
 import { api } from "../utils/api";
 import Landing from "../components/Landing/Head";
 import Footer from "../components/Landing/Footer";
@@ -28,7 +27,6 @@ const Home: NextPage = () => {
           <p className="text-2xl text-white">
             {hello.data ? hello.data.greeting : "Loading tRPC query..."}
           </p>
-          <AuthShowcase />
         </div>
       </main>
     </>
@@ -37,36 +35,3 @@ const Home: NextPage = () => {
 
 export default Home;
 
-const AuthShowcase: React.FC = () => {
-  const { data: sessionData } = useSession();
-
-  const { data: secretMessage } = api.example.getSecretMessage.useQuery(
-    undefined, // no input
-    { enabled: sessionData?.user !== undefined }
-  );
-  const { data: user } = api.example.getAll.useQuery(undefined, {
-    enabled: sessionData?.user !== undefined,
-  });
-
-  return (
-    <div className="flex flex-col items-center justify-center gap-4">
-      <p className="text-center text-2xl text-black">
-        {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
-        {secretMessage && <span> - {sessionData?.user?.id}</span>}
-        {user && (
-          <span>
-            {user.map((u) => (
-              <li key={u.id}>{u.email}</li>
-            ))}
-          </span>
-        )}
-      </p>
-      <button
-        className="rounded-full bg-violet-600 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
-        onClick={sessionData ? () => void signOut() : () => void signIn()}
-      >
-        {sessionData ? "Sign out" : "Sign in"}
-      </button>
-    </div>
-  );
-};
