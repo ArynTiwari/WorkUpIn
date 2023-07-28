227961db0c1ab3998c51b0f2e363ffebaec519ea
Aryan <arutiwari500@gmail.com>
2023-07-29 00:20:49 +0530

    rectification

--- src/components/Landing/ForClient.tsx
+++ src/components/Landing/ForClient.tsx
@@ -1,8 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import Image from "next/image";
-import Button from "../Utils/Button";
 import asset18 from '../../../public/assets/new/asset18.png'
-import confused from '../../../public/assets/unDraw/confused.svg';
 function ForClient() {
   return (
     <>
@@ -14,7 +12,7 @@ function ForClient() {
           <div className="feature-desc flex items-center">
             <h4 className="text-xl font-medium">Unlock Your Creative Potential</h4>
             <h3 className="text-2xl font-semibold">Join WorkUp - The Ultimate Platform for Design Professionals</h3>
-            <p className="text-lg">Are you a talented design professional looking for new opportunities to showcase your skills and connect with clients from around the world? WorkUp offers effortless validation and empowers you to reach new heights in your career.</p>
+            <p className="text-lg text-justify">Are you a talented design professional looking for new opportunities to showcase your skills and connect with clients from around the world? WorkUp offers effortless validation and empowers you to reach new heights in your career.</p>
           </div>
         </div>
       </section>
--- src/components/Landing/ForTalent.tsx
+++ src/components/Landing/ForTalent.tsx
@@ -1,8 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import Image from "next/image";
-import Button from "../Utils/Button";
 import asset21 from '../../../public/assets/new/asset21.png'
-import project from "../../../public/assets/unDraw/project.svg";
 const ForTalent: React.FC = () => {
   return (
     <>
@@ -11,7 +9,7 @@ const ForTalent: React.FC = () => {
           <div className="feature-desc flex items-center">
             <h4 className="text-xl font-medium">Unleash Your Writing Talents</h4>
             <h3 className="text-2xl font-semibold">WorkUp - Your Go-To Platform for Content Writers</h3>
-            <p className="text-lg">Are you a skilled content writer seeking exciting opportunities to showcase your creativity and collaborate with clients from all corners of the globe? WorkUp provides a seamless validation process and empowers you to excel in your writing career.</p>
+            <p className="text-lg text-justify">Are you a skilled content writer seeking exciting opportunities to showcase your creativity and collaborate with clients from all corners of the globe? WorkUp provides a seamless validation process and empowers you to excel in your writing career.</p>
           </div>
           <div className="feature-image hidden md:block">
             <Image src={asset21} alt="" />
